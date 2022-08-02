7a02718d5f09a56920bb2f1dc9c61c4b8b6aa934
JeddAlcalde <jeddalcalde@gmail.com>
2022-08-02 18:01:48 -0400

    Add Base Code
    Base code added to index.html and script.js
    no debugging complete yet and testing is yet to
    happen but base logic works, still getting used
    to JavaScript syntax. README.md needs to be
    updated.

---
+++ script.js
@@ -0,0 +1,80 @@
+function getComputerChoice(){
+    let num = Math.random() * (3-1) + 1;
+
+    if (num == 1){
+        return "rock";
+    } 
+    else if(num == 2){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    let winner = "";
+
+    if(playerSelection == "rock"){
+        if(computerSelection == "rock"){
+            winner = "No one";
+        }
+        else if(computerSelection == "paper"){
+            winner = "computer";
+        }
+        else{
+            winner = "player";
+        }
+    }
+
+    else if(playerSelection == "paper"){
+        if(computerSelection == "rock"){
+            winner = "player";
+        }
+        else if(computerSelection == "paper"){
+            winner = "No one";
+        }
+        else{
+            winner = "computer";
+        }
+    }
+
+    else{
+        if(computerSelection == "rock"){
+            winner = "computer";
+        }
+        else if(computerSelection == "paper"){
+            winner = "player";
+        }
+        else{
+            winner = "No one";
+        }
+    }
+
+    return winner;
+}
+
+function game(){
+    let cpuScore = 0;
+    let playerScore = 0;
+    for (let i = 0; i < 5; i++){
+        let win = playRound(playerSelection, computerSelection)
+        
+        if(win == "computer"){
+            cpuScore++;
+        }
+        else if(win == "player"){
+            playerScore++;
+        }
+        else{
+        }
+        console.log("The winner is " + win)
+    }
+    console.log("CPU Wins : " + cpuScore.toString())
+    console.log("Player Wins : " + playerScore.toString())
+}
+
+let rawInput = prompt("Please enter either \"rock\", \"paper\", or \"scissors\"");
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
